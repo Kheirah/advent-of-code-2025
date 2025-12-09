5033a8afadb2c9f0280b0648e757ff492588b8a2
Eric <eric@spiritsdontfly.com>
2025-12-09 12:31:22 +0100

    Add example and solution files for Day 8
    - Created an example file `08_example.txt` containing a list of junction box coordinates.
    - Implemented the first part of the solution in `08_solution_part_1.js`, which calculates distances between junction boxes and groups them based on proximity.
    - Developed the second part of the solution in `08_solution_part_2.js`, which identifies the last two junctions and computes a specific product based on their coordinates.

---
+++ 08_day/08_solution_part_2.js
@@ -0,0 +1,71 @@
+import fs from "node:fs";
+
+const input = fs.readFileSync("08_input.txt", "utf8");
+const array = input.split("\n");
+const junctionBoxes = array.map((line) => line.split(",").map(Number));
+
+const distances = [];
+
+for (let i = 0; i < junctionBoxes.length - 1; i++) {
+	for (let j = i + 1; j < junctionBoxes.length; j++) {
+		const xDistance = Math.abs(junctionBoxes[i][0] - junctionBoxes[j][0]) ** 2;
+		const yDistance = Math.abs(junctionBoxes[i][1] - junctionBoxes[j][1]) ** 2;
+		const zDistance = Math.abs(junctionBoxes[i][2] - junctionBoxes[j][2]) ** 2;
+		const distance = Math.sqrt(xDistance + yDistance + zDistance);
+		const junction = { j1: junctionBoxes[i], j2: junctionBoxes[j], distance };
+		distances.push(junction);
+	}
+}
+
+const sortedDistances = distances
+	.slice()
+	.sort((a, b) => a.distance - b.distance);
+
+const size = sortedDistances.length;
+const groups = [];
+let groupName = 0;
+const lastTwoJunctions = [];
+
+for (let i = 0; i < size; i++) {
+	const j1 = sortedDistances[i].j1;
+	const j2 = sortedDistances[i].j2;
+	const { exists: isJ1InGroup, index: j1Index } = isInGroup(groups, j1);
+	const { exists: isJ2InGroup, index: j2Index } = isInGroup(groups, j2);
+	if (!isJ1InGroup && !isJ2InGroup) {
+		groups.push({ name: `group-${groupName++}`, junctions: [j1, j2] });
+	} else {
+		if (isJ1InGroup && !isJ2InGroup) {
+			groups[j1Index].junctions.push(j2);
+		}
+		if (isJ2InGroup && !isJ1InGroup) {
+			groups[j2Index].junctions.push(j1);
+		}
+		if (isJ1InGroup && isJ2InGroup) {
+			if (j1Index !== j2Index) {
+				groups[j1Index].junctions.push(...groups[j2Index].junctions);
+				groups.splice(j2Index, 1);
+			} else {
+				continue;
+			}
+		}
+	}
+	lastTwoJunctions[0] = j1;
+	lastTwoJunctions[1] = j2;
+}
+
+function isInGroup(g, [x, y, z]) {
+	for (let i = 0; i < g.length; i++) {
+		const { junctions } = g[i];
+		for (let j = 0; j < junctions.length; j++) {
+			const [jx, jy, jz] = junctions[j];
+			if (x === jx && y === jy && z === jz) {
+				return { exists: true, index: i };
+			}
+		}
+	}
+	return { exists: false, index: undefined };
+}
+
+console.log(lastTwoJunctions[0][0] * lastTwoJunctions[1][0]);
+
+/* 8520040659 is correct */
